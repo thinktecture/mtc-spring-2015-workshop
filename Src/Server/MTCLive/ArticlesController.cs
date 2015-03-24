deadd798505f6cfd200474e2bd5e28caf65d00e3
Christian Weyer <christian.weyer@thinktecture.com>
2015-03-24 11:07:43 +0100

    .NET Web API Mini Sample

---
+++ Src/Server/MTCLive/ArticlesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace MTCLive
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+
+    public class ArticlesController : ApiController
+    {
+        [ActionName("ping")]
+        public string GetPing()
+        {
+            return "OK";
+        }
+
+        [ActionName("item")]
+        public Product GetProduct()
+        {
+            return  new Product{Id = 42, Title = "Eh klar..."};
+        }
+    }
+}
---
+++ Src/Server/MTCLive/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http.Formatting;
+using System.Web.Http;
+using Owin;
+
+namespace MTCLive
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            config.Routes.MapHttpRoute("defaultApi", "api/{controller}/{action}");
+
+            config.Formatters.Clear();
+
+            var formatter = new JsonMediaTypeFormatter();
+            config.Formatters.Add(formatter);
+
+            app.UseWebApi(config);
+        }
+    }
+}
